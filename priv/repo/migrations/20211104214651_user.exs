370e73a15b8db6283cf136a1bec5273d5381d640
chase.coche <chasecoche@ucwv.edu>
2021-11-04 18:08:56 -0400

    This is a commit message, nothing to see here

--- priv/repo/migrations/20211104214651_user.exs
+++ priv/repo/migrations/20211104214651_user.exs
@@ -2,6 +2,18 @@ defmodule FinalProject.Repo.Migrations.User do
   use Ecto.Migration
 
   def change do
+    create table("users") do
+      add :name, :string, null: false
+      add :email, :string, null: false
+      # Use BCrypt
+      add :password_hash, :string, null: false
+      add :game_style, :string, null: true
+      add :player_style, :string, null: true
+      timestamps()
+    end
 
+    alter table("games") do
+      add :user_id, references("users")
+    end
   end
 end
